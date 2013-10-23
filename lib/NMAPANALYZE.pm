14d95437b53180d2e29d0960840b673eeff6e4e6
Peter Kempf <pk@ekse.de>
2013-10-23 09:15:11 +0200

    * Aufgenommen

---
+++ lib/NMAPANALYZE.pm
@@ -0,0 +1,226 @@
+package PROGRAMM;
+
+#-------------------------------------------------------------------------------------------------
+# Letzte Aenderung:     $Date: 2013-04-05 13:53:40 +0200 (Fr, 05 Apr 2013) $
+#                       $Revision: 1003 $
+#                       $Author: xck90n1 $
+#
+# Aufgabe:				- Ausfuehrbarer Code von programm.pl
+#
+# $Id: PROGRAMM.pm 1003 2013-04-05 11:53:40Z xck90n1 $
+# $URL: https://svn.fiducia.de/svn/multicom/trunk/multicom/Framework_OO/lib/PROGRAMM.pm $
+#-------------------------------------------------------------------------------------------------
+
+use 5.004;
+use strict;
+use vars qw($VERSION $SVN $OVERSION);
+
+use constant SVN_ID => '($Id: PROGRAMM.pm 1003 2013-04-05 11:53:40Z xck90n1 $)
+
+$Author: xck90n1 $ 
+
+$Revision: 1003 $ 
+$Date: 2013-04-05 13:53:40 +0200 (Fr, 05 Apr 2013) $ 
+
+$URL: https://svn.fiducia.de/svn/multicom/trunk/multicom/Framework_OO/lib/PROGRAMM.pm $
+
+';
+
+($VERSION = SVN_ID) =~ s/^(.*\$Revision: )([0-9]*)(.*)$/1.0 R$2/ms;
+$SVN      = $VERSION . ' ' . SVN_ID;
+$OVERSION = $VERSION;
+
+use base 'Exporter';
+
+our @EXPORT    = ();
+our @EXPORT_OK = ();
+
+use vars @EXPORT, @EXPORT_OK;
+
+use vars qw(@ISA);
+@ISA = qw();
+
+use Trace;
+use CmdLine;
+use Configuration;
+use DBAccess;
+
+#
+# Module
+#
+use Utils;
+use FindBin qw($Bin $Script $RealBin $RealScript);
+use Date::Format;
+use File::Basename;
+use File::Path qw(mkpath);
+use MIME::Lite;
+use IO::Compress::Gzip qw(gzip $GzipError);
+use LockFile::Simple qw(lock trylock unlock);
+
+#
+# Perl2Exe Compilerdirectiven
+#
+#perl2exe_include DBD::DB2
+
+#
+# Konstantendefinition
+#
+
+#
+# Variablendefinition
+#
+
+#
+# Methodendefinition
+#
+sub version {
+  my $self     = shift();
+  my $pversion = shift();
+
+  $OVERSION =~ m/^([^\s]*)\sR([0-9]*)$/;
+  my ($oVer, $oRel) = ($1, $2);
+  
+  if (defined($pversion)) {
+    $pversion =~ m/^([^\s]*)\sR([0-9]*)$/;
+    my ($pVer, $pRel) = ($1, $2);
+    $VERSION = $oRel > $pRel ? "$pVer R$oRel" : "$pVer R$pRel";
+  }
+
+  return wantarray() ? ($VERSION, $OVERSION) : $VERSION;
+}
+
+sub new {
+  #################################################################
+  #     Legt ein neues Objekt an
+  my $self  = shift;
+  my $class = ref($self) || $self;
+  my @args  = @_;
+
+  my $ptr = {};
+  bless $ptr, $class;
+  $ptr->_init(@args);
+
+  return $ptr;
+}
+
+sub _init {
+  #################################################################
+  #   Initialisiert ein neues Objekt
+  my $self = shift;
+  my @args = @_;
+
+  $self->{Startzeit} = time();
+  
+  if (Configuration->config('Prg', 'Plugin')) {
+
+    # refs ausschalten wg. dyn. Proceduren
+    no strict 'refs';
+    my %plugin = ();
+
+    # Bearbeiten aller Erweiterungsmodule die in der INI-Date
+    # in Sektion [Prg] unter "Plugin =" definiert sind
+    foreach (split(/ /, Configuration->config('Prg', 'Plugin'))) {
+
+      # Falls ein Modul existiert
+      if (-e "$self->{Pfad}/plugins/${_}.pm") {
+
+        # Einbinden des Moduls
+        require $_ . '.pm';
+        $_->import();
+
+        # Initialisieren des Moduls, falls es eine eigene Sektion
+        # [<Modulname>] fuer das Module in der INI-Datei gibt
+        $plugin{$_} = eval {$_->new(Configuration->config('Plugin ' . $_))};
+        eval {
+          $plugin{$_} ? $plugin{$_}->DESTROY : ($_ . '::DESTROY')->()
+            if (CmdLine->option('erase'));
+        };
+      }
+    }
+    use strict;
+  }
+
+  # Module::Refresh->refresh;
+  
+  # Test der benoetigten INI-Variablen
+  # DB-Zugriff
+  #$self->Exit(1, 0, 0x08002, 'DB', 'MC_DB')                if (!defined($self->config('DB', 'MC_DB')));
+  #$self->Exit(1, 0, 0x08002, 'DB', 'FID_DB')               if (!defined($self->config('DB', 'FID_DB')));
+
+  # Ergebnisausgabe und Sicherung
+  #$self->Exit(1, 0, 0x08002, 'Ausgabe', 'Log')             if (!defined($self->config('Ausgabe', 'Log')));
+  #$self->Exit(1, 0, 0x08002, 'Ausgabe', 'Err')             if (!defined($self->config('Ausgabe', 'Err')));
+  #$self->Exit(1, 0, 0x08002, 'Ausgabe', 'Out')             if (!defined($self->config('Ausgabe', 'Out')));
+  #$self->Exit(1, 0, 0x08002, 'Ausgabe', 'SICHERUNG')       if (!defined($self->config('Ausgabe', 'SICHERUNG')));
+
+  if (Configuration->config('Prg', 'LockFile')) {
+    $self->{LockFile} = Utils::extendString(Configuration->config('Prg', 'LockFile'), "BIN|$Bin|SCRIPT|" . uc($Script));
+    $self->{Lock} = LockFile::Simple->make(-max => 5, -delay => 1, -format => '%f', -autoclean => 1, -stale => 1, -wfunc => undef);
+    my $errtxt;
+    $SIG{'__WARN__'} = sub {$errtxt = $_[0]};
+    my $lockerg = $self->{Lock}->trylock($self->{LockFile});
+    undef($SIG{'__WARN__'});
+    if (defined($errtxt)) {
+      $errtxt =~ s/^(.*) .+ .+ line [0-9]+.*$/$1/;
+      chomp($errtxt);
+      Trace->Trc('S', 1, 0x00012, $errtxt) if defined($errtxt);
+    }
+    if (!$lockerg) {
+      Trace->Exit(0, 1, 0x00013, Configuration->prg, $self->{LockFile})
+    } else {
+      Trace->Trc('S', 1, 0x00014, $self->{LockFile})
+    }
+  }
+  $self->{AutoCommit} = Configuration->config('DB', 'AUTOCOMMIT') || 0;
+}
+
+sub DESTROY {
+  #################################################################
+  #     Zerstoert das Objekt an
+  my $self = shift;
+  my ($rc, $sig) = (0,0);
+  $rc  = ($? >> 8);
+  $sig = $? & 127;
+  if ($@ || $rc != 0 || $sig != 0) {
+    my ( $routine, $i ) = ( ( caller(0) )[3] . ':', 0 );
+    while ( defined( caller( ++$i ) ) ) {
+      $routine .= ( caller($i) )[3] . '(' . ( caller( $i - 1 ) )[2] . '):';
+    }
+    Trace->Trc('S', 1, 0x00007, "$routine $@ $! $?");
+    Trace->Log('Log', 0x10013, $@, $!, $?);
+  }
+  for my $parent (@ISA) {
+    if ( my $coderef = $self->can( $parent . "::DESTROY" ) ) {
+      $self->$coderef();
+    }
+  }
+  # Eigentlich nicht noetig, da -autoclean => 1
+  if ($self->{Lock}) {$self->{Lock}->unlock($self->{LockFile})}
+}
+
+sub autocommit {
+  #################################################################
+  #     Ggf. Zyklisches Absetzen des Commits
+  #
+  my $self = shift;
+
+  my $merker          = $self->{subroutine};
+  $self->{subroutine} = (caller(0))[3];
+  Trace->Trc('S', 1, 0x00001, $self->{subroutine}, $self->argument(0));
+
+  if ($self->{AutoCommit} > 0) {
+    if ($self->{AutoCommit}-- == 0) {
+      $self->{AutoCommit} = Configuration->config('DB', 'AUTOCOMMIT') || 0;
+      DBAccess->commit() or Trace->Exit(0x104, 0, "Error: $DBI::errstr");
+    }
+  }
+
+  Trace->Trc( 'S', 1, 0x00002, $self->{subroutine} );
+  $self->{subroutine} = $merker;
+
+  # Explizite Uebergabe des Returncodes noetig, da sonst ein Fehler auftritt
+  return 1;
+}
+
+
+1;
---
+++ nmapanalyse.pl
@@ -0,0 +1,166 @@
+eval 'exec perl -wS $0 ${1+"$@"}'
+  if 0;
+
+#-------------------------------------------------------------------------------------------------
+# Letzte Aenderung:     $Date: 2013-04-05 13:53:40 +0200 (Fr, 05 Apr 2013) $
+#                       $Revision: 1003 $
+#                       $Author: xck90n1 $
+#
+# Aufgabe:				- was tun
+#
+# $Id: programm.pl 1003 2013-04-05 11:53:40Z xck90n1 $
+# $URL: https://svn.fiducia.de/svn/multicom/trunk/multicom/Framework_OO/programm.pl $
+#-------------------------------------------------------------------------------------------------
+
+# Letzte Aenderung: Transaktionslogik in SPLIT.pm eingefuehrt
+
+use 5.010;
+use strict;
+use vars qw($VERSION $SVN);
+
+use constant SVN_ID => '($Id: programm.pl 1003 2013-04-05 11:53:40Z xck90n1 $)
+
+$Author: xck90n1 $ 
+
+$Revision: 1003 $ 
+$Date: 2013-04-05 13:53:40 +0200 (Fr, 05 Apr 2013) $ 
+
+$URL: https://svn.fiducia.de/svn/multicom/trunk/multicom/Framework_OO/programm.pl $
+
+';
+
+# Extraktion der Versionsinfo aus der SVN Revision
+($VERSION = SVN_ID) =~ s/^(.*\$Revision: )([0-9]*)(.*)$/1.0 R$2/ms;
+$SVN = $VERSION . ' ' . SVN_ID;
+
+$| = 1;
+
+# use lib $Bin . "/lib";       # fuer Aufruf mit voll qualifiziertem Pfad noetig
+use lib "./lib";    # fuer perl2exe noetig
+
+#use lib $Bin . "/lib/SPLIT"; # fuer Aufruf mit voll qualifiziertem Pfad noetig
+#use lib "./lib/SPLIT";       # fuer perl2exe noetig
+
+#
+# Module
+#
+use CmdLine;
+use Trace;
+use Configuration;
+use DBAccess;
+
+use PROGRAMM;
+use PROGRAMM::Modul1;
+use PROGRAMM::Modul2;
+
+use Fcntl;
+use FindBin qw($Bin $Script $RealBin $RealScript);
+
+#
+# Variablendefinition
+#
+
+#
+# Objektdefinition
+#
+
+# Option-Objekt: Liest und speichert die Kommandozeilenparameter
+my $cmdLine = CmdLine->new('Client'  => 'client:s',
+                           'Region'  => 'region:s',
+                           'Partner' => 'partner:s');
+$VERSION = $cmdLine->version($VERSION);
+
+# Trace-Objekt: Liest und speichert die Meldungstexte; gibt Tracemeldungen aus
+my $trace = Trace->new();
+$VERSION = $trace->version($VERSION);
+
+# Config-Objekt: Liest und speichert die Initialisierungsdatei
+my $config = Configuration->new();
+$VERSION = $config->version($VERSION);
+
+# Datenbank-Objekt: Regelt dei Datenbankzugriffe
+my $dbaccess = DBAccess->new();
+$VERSION = $dbaccess->version($VERSION);
+
+# Kopie des Fehlerkanals erstellen zur gelegentlichen Abschaltung
+no warnings;
+sysopen(MYERR, "&STDERR", O_WRONLY);
+use warnings;
+
+#
+#################################################################
+## main
+##################################################################
+#
+$trace->Trc('S', 1, 0x00001, $config->prg, 
+                             $VERSION .
+                             " (" . $$ . ") " . 
+                             "Test: " . $trace->test .
+                             "  Parameter: " . $cmdLine->{ArgStrg});
+
+# Test der Komandozeilenparameter
+if (!$cmdLine->argument(0) || !$cmdLine->argument(1) ||
+     $cmdLine->option('Help')|| $cmdLine->option('Version')) {
+  $cmdLine->usage;
+  if ($cmdLine->option('Help') || $cmdLine->option('Version')) {
+    $trace->Exit(0, 1, 0x00002, $config->prg, $VERSION);
+  }
+  $trace->Exit(1, 0, 0x08003, $cmdLine->{ArgStrg});
+}
+
+my $prg;
+eval {$prg = PROGRAMM->new()};
+if ($@) {
+  $prg->Exit(0, 1, 0x0ffff, $prg->prg, $VERSION);
+}
+$VERSION = $prg->version($VERSION);
+$prg->set_pers_Var($prg->config('DB', 'FID_DB').'.fid_config', 'Start');
+
+my $a;
+my %b;
+
+$a = $prg->config('Ausgabe', 'Log');
+$a = $prg->config('Ausgabe');
+$a = $prg->config();
+$a = $prg->config('Ausgabe', 'Frog');
+$a = $prg->config('Rausgabe', 'Frog');
+$a = $prg->config('Rausgabe');
+
+%b = $prg->config('Ausgabe', 'Log');
+%b = $prg->config('Ausgabe');
+%b = $prg->config();
+%b = $prg->config('Ausgabe', 'Frog');
+%b = $prg->config('Rausgabe', 'Frog');
+%b = $prg->config('Rausgabe');
+
+  
+
+# Test der benoetigten Kommandline-Variablen
+if ($prg->argument(0)) {
+  $prg->{Eingabemaske} = $prg->argument(0)
+} else {
+  $prg->{Eingabemaske} = $prg->config('Eingabe', 'Fusionsliste')
+}
+
+# Anlegen der Dateien zum Log, Error-Log und Ausgabeliste sowie FMC  
+$prg->Log('Log', $prg->config('Ausgabe', 'Log')) or $prg->Exit(1, 0, 0x00010, Utils::extendString($prg->config('Ausgabe', 'Log')));
+if (my $logrc = $prg->Log('FMC', $prg->config('Ausgabe', 'FMC'), '0111')) {
+  if ($logrc eq 'N') {
+    $prg->Log('FMC', "ERSTEINTRAG") or $prg->Exit(1, 0, 0x00010, Utils::extendString($prg->config('Ausgabe', 'FMC')));
+  }
+} else {
+  $prg->Exit(1, 0, 0x00010, $prg->config('Ausgabe', 'FMC'))
+}
+
+#--------------------------------------------------------------
+# PRGRAMM-Start
+#--------------------------------------------------------------
+$prg->Log('Log', 0x10000, $prg->version());
+$prg->Log('Log', 0x10001, $prg->prg, '', $cmdLine->{ArgStrg});
+
+
+
+$prg->set_pers_Var($prg->config('DB', 'FID_DB').'.fid_config', 'Ende '.$cmdLine->{ArgStrg});
+$prg->Exit(0, 1, 0x00002, $prg->prg, $VERSION);
+
+exit 1;
